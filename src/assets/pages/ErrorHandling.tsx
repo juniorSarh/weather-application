1a37cedb5b0a4b1a205df6c36d6316e532d1934d
juniorSarh <sabelohgumede@gmail.com>
2026-01-09 10:11:20 +0200

    redirecting on page 404

--- src/assets/pages/ErrorHandling.tsx
+++ src/assets/pages/ErrorHandling.tsx
@@ -1,10 +1,13 @@
 
 import './errorhandling.modules.css'
+import { Link } from 'react-router-dom'
 export default function ErrorHandling() {
   return (
     <div className="errorHandling">
       <h1>404</h1>
       <p>Page not found</p>
+      <Link to='/'>Go back to home</Link>
+      
     </div>
   )
 }
