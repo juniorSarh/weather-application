466d3ea4a248572c9c55726de1d6aeb49e713695
juniorSarh <sabelohgumede@gmail.com>
2026-01-09 10:05:26 +0200

    added page 404 for error handling

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './assets/pages/Home'
 import Locations from './assets/pages/Locations'
 import Settings from './assets/pages/Settings'
+import ErrorHandling from './assets/pages/ErrorHandling'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
     <Route path='' element={<Home/>}/>
     <Route path='/locations' element={<Locations/>}/>
     <Route path='/settings' element={<Settings/>}/> 
+    <Route path='*' element={<ErrorHandling/>}/>
      </Routes>
     </>
   )
--- src/assets/pages/ErrorHandling.tsx
+++ src/assets/pages/ErrorHandling.tsx
@@ -1,9 +1,10 @@
 
-
+import './errorhandling.modules.css'
 export default function ErrorHandling() {
   return (
-    <div>
-      
+    <div className="errorHandling">
+      <h1>404</h1>
+      <p>Page not found</p>
     </div>
   )
 }
